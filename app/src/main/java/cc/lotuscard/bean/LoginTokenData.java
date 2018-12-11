1159093a6d32372781e09c5c38408bdbde09ad11
zjl7577 <1097919195@qq.com>
2018-12-11 10:53:54 +0800

    登录字段优化

--- app/src/main/java/cc/lotuscard/bean/LoginTokenData.java
+++ app/src/main/java/cc/lotuscard/bean/LoginTokenData.java
@@ -13,7 +13,6 @@ public class LoginTokenData {
 
     private String access_token;
     private String token_type;
-    private int expires_in;
 
     public String getAccess_token() {
         return access_token;
@@ -29,13 +28,5 @@ public class LoginTokenData {
 
     public void setToken_type(String token_type) {
         this.token_type = token_type;
-    }
-
-    public int getExpires_in() {
-        return expires_in;
-    }
-
-    public void setExpires_in(int expires_in) {
-        this.expires_in = expires_in;
     }
 }
